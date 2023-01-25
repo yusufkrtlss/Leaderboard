6c1a710b760da5324ffb9ed57ffad0c0137f239a
yusufkrtlss <krtlss.yusuf@gmail.com>
2023-01-25 12:28:34 +0300

    First Commit

---
+++ Leaderboard/Program.cs
@@ -0,0 +1,40 @@
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Serilog
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("log.txt")
+    .WriteTo.Seq("http://localhost:5341/")
+    .CreateLogger();
+builder.Host.UseSerilog();
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
